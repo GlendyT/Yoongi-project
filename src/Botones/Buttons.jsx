e8d8806ee5474984f5e018c3c8a4d475310cf179
GlendyT <glendytuyuc@gmail.com>
2024-02-22 08:19:29 -0600

    background

--- src/Botones/Buttons.jsx
+++ src/Botones/Buttons.jsx
@@ -37,7 +37,7 @@ export default function Buttons({ htmlToImageConvert, handleLogout}) {
       <button 
       className="w-full bg-white text-black cursor-pointer p-3 font-bold font-pixel ">
         <a
-          href="https://twitter.com/intent/tweet?text=Happy%20Suga%20day&url=https://twitter.com/bts_bighit&hashtags=VerseForSuga&hashtags=PoetryForSuga&hashtags=intothesugaverse&hashtags=IntoTheSugaVerse&hashtags=verseforsugabyarmy&hashtags=VerseForSuga
+          href="https://twitter.com/intent/tweet?text=Happy%20Suga%20day&url=https://yoongi-project.vercel.app/&hashtags=VerseForSuga&hashtags=PoetryForSuga&hashtags=intothesugaverse&hashtags=IntoTheSugaVerse&hashtags=verseforsugabyarmy&hashtags=VerseForSuga
         "
           target="_blank"
         >
